a920e4ae8f83158fa0a32354c444aafec78605e0
asd2d <872122@mail.ru>
2023-03-21 22:34:56 +0300

    добавил форму для ввода имени

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//phpinfo();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="index.php" method="post">
+        <p>
+            <label for="name">Введите ваше имя:</label><br>
+            <input type="text" name="name" id="name">
+        </p>
+    </form>
+
+</body>
+</html>
