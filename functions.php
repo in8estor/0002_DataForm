e4f7237990fb588b9cbad28332b18e4ecbed8188
asd2d <872122@mail.ru>
2023-03-29 23:25:16 +0300

    добавил в проект файлы конфигурации docker

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
-function save_message(){
+/**function save_message(){
     global $connect;
         $firstName = mysqli_real_escape_string($connect, $_POST['firstname']);
         $email = mysqli_real_escape_string($connect, $_POST['email']);
         $query = "INSERT INTO GuestBookTable (firstname, email) VALUES ('$firstName', '$email')";
         mysqli_query($connect, $query);
 }
+
+*/
